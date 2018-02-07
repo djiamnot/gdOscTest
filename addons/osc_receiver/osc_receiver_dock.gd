4f7e42069cef4dd47a04678b7c6994dfea4d390b
Michał Seta <djiamnot@gmail.com>
2018-02-07 08:39:24 -0500

    some tweaks

--- addons/osc_receiver/osc_receiver_dock.gd
+++ addons/osc_receiver/osc_receiver_dock.gd
@@ -2,6 +2,7 @@ tool
 extends EditorPlugin
 
 var dock # A class member to hold the dock during the plugin lifecycle
+var osc_listener
 
 func _enter_tree():
     # Initialization of the plugin goes here
@@ -11,9 +12,16 @@ func _enter_tree():
     # Add the loaded scene to the docks:
     add_control_to_dock( DOCK_SLOT_LEFT_UL, dock)
     # Note that LEFT_UL means the left of the editor, upper-left dock
+    osc_listener  = OSCListener.new() # port 18002 by default
+    # receiving port can be changed:
+    osc_listener.set_port(18004)
+    osc_listener.connect("osc_message", self, "_on_osc_msg")
 
 func _exit_tree():
     # Clean-up of the plugin goes here
     # Remove the scene from the docks:
     remove_control_from_docks( dock ) # Remove the dock
     dock.free() # Erase the control from the memory
+
+func _on_osc_msg(val):
+    print("dock received: ", val)
