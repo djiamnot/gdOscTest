ac0ef172ca871e9c40c74dd01db9f94e2c13501a
Michał Seta <djiamnot@gmail.com>
2018-03-09 09:30:06 -0500

    towards an OSC example

--- scripts/listener.gd
+++ scripts/listener.gd
@@ -8,11 +8,12 @@ var osc_ready = false
 var osc_listener
 
 func _ready():
-    pass
-    # osc_listener  = OSCListener.new() # port 18002 by default
-    # receiving port can be changed:
+    # set_port(18004)
+    osc_listener  = OSCListener.new() # port 18002 by default
+    #  port can be changed:
     # osc_listener.set_port(18004)
-    # osc_listener.connect("osc_message", self, "_on_osc_msg")
+    osc_listener.connect("osc_message", self, "_on_osc_msg")
     
 func _on_osc_msg(val):
+    print("got message ", val)
     emit_signal("osc_message", val)
