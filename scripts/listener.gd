f603664e6da1443e016337bf8e244f9cae100581
Michał Seta <djiamnot@gmail.com>
2018-03-10 18:04:50 -0500

    cleanup and refactor

--- scripts/listener.gd
+++ scripts/listener.gd
@@ -8,12 +8,10 @@ var osc_ready = false
 var osc_listener
 
 func _ready():
-    # set_port(18004)
     osc_listener  = OSCListener.new() # port 18002 by default
     #  port can be changed:
-    # osc_listener.set_port(18004)
+    # osc_listener.setPort(18004)
     osc_listener.connect("osc_message", self, "_on_osc_msg")
     
 func _on_osc_msg(val):
-    print("got message ", val)
     emit_signal("osc_message", val)
