918491fc08c2e4e2b479154cbb690b38f3db9f1e
Michał Seta <djiamnot@gmail.com>
2018-03-10 18:05:51 -0500

    maintenance

--- addons/osc_receiver/OSCserver_button.gd
+++ addons/osc_receiver/OSCserver_button.gd
@@ -18,5 +18,5 @@ func _on_osc_msg(val):
 func _on_button_pressed():
     # osc_listener  = OSCListener.new() # port 18002 by default
     # receiving port can be changed:
-    # osc_listener.set_port(18004)
+    # osc_listener.setPort(18004)
     # osc_listener.connect("osc_message", self, "_on_osc_msg")
