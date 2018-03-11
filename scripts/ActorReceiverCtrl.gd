0e4f30c80414def3b73eb5aa2ae070698ab8ca6d
Michał Seta <djiamnot@gmail.com>
2018-03-10 22:47:08 -0500

    update some documentation

--- scripts/ActorReceiverCtrl.gd
+++ scripts/ActorReceiverCtrl.gd
@@ -7,21 +7,18 @@ func _ready():
     # Initialization here
     set_process_input(true)
     set_process(true)
+    # here we get the node where OSC communication is received.
+    # it emits a 'osc_message' signal
     osc = get_parent_spatial()
-#    osc = get_node("OSCListener")
     osc.connect("osc_message", self, "_on_osc_message")
-    var osc_tool = get_tree()
-    print(osc_tool)
 
 func _on_osc_message(val):
     print(get_name(), " ", val)
-    # print(val[0].right(1))
+    # test for my name
     if val[0].right(1).match(get_name()):
-        print("matching name")
+        print("matching name -- _on_osc_message")
         if val[1] == "pos":
             translate(Vector3(val[2], val[3], val[4]))
-    
-
         
 func _process(delta):
     pass
@@ -34,7 +31,7 @@ func _on_OSCListener_osc_message(val):
     print(get_name(), " ", val)
     print(val[0].right(1))
     if val[0].right(1).match(get_name()):
-        print("matching name")
+        print("matching name _on_OSClistener_osc_message")
         if val[1] == "pos":
             print(self, " ", self.translation)
             translate(Vector3(2,0,0))
--- scripts/listener.gd
+++ scripts/listener.gd
@@ -6,12 +6,19 @@ signal osc_message()
 var osc_ready = false
 
 var osc_listener
-
+# This is how we instantiate the main receiver class. It will probably exist in your scene and 
+# interested object will connect to its signal
 func _ready():
     osc_listener  = OSCListener.new() # port 18002 by default
     #  port can be changed:
     # osc_listener.setPort(18004)
-    osc_listener.connect("osc_message", self, "_on_osc_msg")
     
+    # osc_message is a signal emitted on every reception of OSC message
+    osc_listener.connect("osc_message", self, "_on_osc_msg")
+    # there is also 'osc_ready' signal for your c onvenience, emited as soon as the OSC receiver is
+    # successfully instantiated
+    
+# I like to forward the message (at this time, I am considering this my top-level node and scenes
+# will be instantiated below it)
 func _on_osc_msg(val):
     emit_signal("osc_message", val)
